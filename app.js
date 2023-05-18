18f960418ee62e17396320e90df8580b657a8fe4
cristian <cristian.mele.73@gmail.com>
2023-05-18 08:19:58 -0300

    agregadas varias funcionalidades

--- app.js
+++ app.js
@@ -1,33 +1,57 @@
 const productos=[
     {
         nombre:"naruto" ,
-        tamanio: "grande",
-        precio: 10000
+        categorias:["naruto"],
+        precio: 12000,
+        imagen:"https://media.sketchfab.com/models/93174ca7b0bb4f2d8ddf00021854f2cb/thumbnails/ad762fd30c89458bba7ca4a8a67e3032/515eb1f466794bd186244eea8157e1d7.jpeg"
     } ,
     {
         nombre:"sasuke" ,
-        tamanio: "mediano",
-        precio: 7000
-    },
-    {
-        nombre:"itachi" ,
-        tamanio: "mediano",
-        precio: 5500
-    },
-    {
-        nombre:"sakura" ,
-        tamanio: "chico",
-        precio: 3000
-    },
-    {
-        nombre:"gara" ,
-        tamanio: "chico",
-        precio: 2000
+        categorias:["naruto"],
+        precio: 7000,
+        imagen:"https://img1.cgtrader.com/items/4068141/159d5fd3a7/large/sasuke-uchiha-from-battle-with-itachi-3d-model-159d5fd3a7.jpg"
     },
     {
         nombre:"kakashi" ,
-        tamanio: "grande",
-        precio: 8000
+        categorias:["naruto"],
+        precio: 8800,
+        imagen:"https://img1.cgtrader.com/items/2659360/3b8b8f950e/large/kakashi-hatake-from-naruto-3d-model-stl.jpg"
+    },
+    {
+        nombre:"goku" ,
+        categorias:["dragonball"],
+        precio: 10000,
+        imagen:"https://i.ebayimg.com/images/g/McYAAOSwg5ZkDDTm/s-l1600.jpg"
+    },
+    {
+        nombre:"vegeta" ,
+        categorias:["dragonball"],
+        precio: 8000,
+        imagen: "https://cdn.meshplorer.com/media/25/35/78/975ac89eaad70e3be343a5c6a7.jpg"
+    },
+    {
+        nombre:"gohan" ,
+        categorias:["dragonball"],
+        precio: 7800,
+        imagen:"https://i.pinimg.com/736x/f4/5c/2b/f45c2b1d98bb0b7787979b6011e7cb51.jpg"
+    },
+    {
+        nombre:"luffy" ,
+        categorias:["onepiece"],
+        precio: 8500,
+        imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbmCm38TORJCwNwxB9MnR5BQUXpUK_E1hE1g&usqp=CAU"
+    },
+    {
+        nombre:"zoro" ,
+        categorias:["onepiece"],
+        precio: 6300,
+        imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVFVHH7lfXy0xrXkuMtafZ8UG5B-FFVq65ew&usqp=CAU"
+    },
+    {
+        nombre:"sanji" ,
+        categorias:["onepiece"],
+        precio: 4800,
+        imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRUOJJouub8j_WJgw2MbuIWdCRdGtcQwZcWA&usqp=CAU"
     }
     
 ]
@@ -51,7 +75,7 @@ var usuarios=[
     }
 ]
 
-var carrito=[{nombre:"lala",precio:1500,cantidad:2,total:3000}, {nombre:"lolo",precio:1500,cantidad:2,total:3000}]
+var carrito=[{nombre:"lala",precio:1500,cantidad:2,total:3000, imagen:"https://i.blogs.es/6f076c/naruto/840_560.jpeg"}, {nombre:"lolo",precio:1500,cantidad:2,total:3000, imagen:"https://i.blogs.es/6f076c/naruto/840_560.jpeg"}]
 function setearUsuario(){
     const usuarioConectado=document.querySelector(".nav-loggin")
     if(sessionStorage.getItem("usuario")!=null){
@@ -61,6 +85,20 @@ function setearUsuario(){
     }
 }
 setearUsuario()
+habilitarCompra()
+function habilitarCompra(){
+    const textoDesconectado= document.querySelector(".txt-desconectado")
+    const botonComprar=document.querySelector("#comprar")
+    if(sessionStorage.getItem("usuario")!=null){
+        textoDesconectado.classList.add("oculto")
+        botonComprar.classList.remove("disabled")
+    }else{
+        botonComprar.classList.add("disabled")
+        textoDesconectado.classList.remove("oculto")
+    }
+    
+}
+
 
 
 
@@ -105,6 +143,7 @@ if(document.querySelector('#loggout')!=null){
             document.querySelector("#error").classList.remove ("oculto")
             
         } 
+        habilitarCompra();
         sesionIniciada();
     }
     /* FIN Funcion para validar usuario y contrasenia y poner el usuario en sesionStorage */
@@ -159,6 +198,8 @@ function sesionIniciada(){
     }
     mostrarCarrito()
     setearUsuario()
+    habilitarCompra();
+    
 }
 
 /* ------------------------------- MENSAJE DE BIENVENIDA---------------------------- */
@@ -182,11 +223,14 @@ FIN DEL HTML LOGGIN
 /*==============================================================================================================
 INICIO CODIGO CARRITO
 ================================================================================================================ */
+
+/* Funcion para mostrar el carrito! */
 function mostrarCarrito(){
     const mostrarCarrito=document.querySelector(".carrito")
-    mostrarCarrito.innerHTML=''
+    mostrarCarrito.innerHTML=' '
     carrito.forEach(e =>{
-        mostrarCarrito.innerHTML+=`<div class="row mb-1">
+        mostrarCarrito.innerHTML+=` <div class="row mb-1">
+                                    <img class="col-2 img-mini" src="${e.imagen}">
                                     <div class="col-3 nombre">${e.nombre}</div>
                                     <button class="btn btn-danger col-1 btn-restar">-</button>
                                     <div class="col-1 centrar cantidad">${e.cantidad}</div>
@@ -208,6 +252,8 @@ function mostrarCarrito(){
 
 }
 
+
+/* Funcion para eliminar del carrito */
 function eliminarDelCarrito(element){
     const eliminar=element.querySelector(".nombre").textContent
     carrito.forEach((e,index)=>{
@@ -218,10 +264,10 @@ function eliminarDelCarrito(element){
     mostrarCarrito()
 }
 
+
+/* funcion para sumar y restar cantidades */
 function sumaResta(element, a){
-    
     const nombreProducto=element.querySelector(".nombre").textContent 
-    console.log(nombreProducto)
     if(a=="suma"){
         carrito.forEach((e)=>{
             if(nombreProducto==e.nombre){
@@ -247,6 +293,8 @@ function sumaResta(element, a){
     
 }
 
+
+/* botones del carrito para sumar y restar cantidades */
 if(document.querySelector(".carrito")!=null){
     const carro= document.querySelector(".carrito")
     carro.addEventListener("click",e=>{
@@ -260,9 +308,76 @@ if(document.querySelector(".carrito")!=null){
             eliminarDelCarrito(e.target.parentElement)
         }
     })
+    const vaciar= document.querySelector(".btn-vaciar")
+    vaciar.addEventListener("click", e=>{
+        carrito=[]
+        mostrarCarrito()
+    })
+}
+
+/* Carrito modal para el index y el  catalogo */
+if(document.querySelector(".modal-carrito")!=null){
+    const carro=document.querySelector(".modal-carrito")
+    carro.addEventListener("click", e=>{
+        mostrarCarrito()
+    })
 }
 /*==============================================================================================================
 FIN CODIGO CARRITO
+================================================================================================================ */
+
+
+/*==============================================================================================================
+INICIO CODIGO PRODUCTOS
+================================================================================================================ */
+if(document.querySelector("#catalogo")!=null){
+    const produ= document.querySelector("#productos")
+    productos.forEach(e =>{
+        produ.innerHTML+=`<div class="card m-3 col-4 p-1" style="width: 18rem;">
+                            <img src="${e.imagen}" class="card-img-top" alt="...">                            
+                            <div class="card-body">
+                            <div class="oculto url-img">${e.imagen}</div>
+                                <h5 class="card-title">${e.nombre}</h5>
+                                <p class="card-text">${e.precio}</p>
+                                <button class="btn btn-primary btn-comprar">Agregar al carrito</a>
+                            </div>
+                        </div>`
+    })
+
+    const clickComprar= document.querySelector("#productos")
+    clickComprar.addEventListener("click", e=>{
+        if(e.target.classList.contains("btn-comprar")){
+            agregarCarrito(e.target.parentElement)
+        }
+    })
+
+   
+}
+
+function agregarCarrito(element){
+    var aux=true
+    const nombreElemento=element.querySelector(".card-title").textContent
+    const precioElemento=Number(element.querySelector(".card-text").textContent)
+    
+    carrito.forEach(e =>{
+        if(e.nombre==nombreElemento){
+            e.cantidad++
+            e.total+=precioElemento
+            aux=false
+        }
+    })
+    if(aux){
+        let productoAgregar={ nombre:nombreElemento,
+                            precio:precioElemento,
+                            cantidad:1,
+                            total:precioElemento,
+                            imagen:element.querySelector(".url-img").textContent}
+        carrito.push(productoAgregar)
+    }
+    
+}
+/*==============================================================================================================
+FIN CODIGO PRODUCTOS
 ================================================================================================================ */
 
 
